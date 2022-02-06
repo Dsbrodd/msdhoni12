8ed9d904e0e530cb277ada0ba67a90a0b255527b
Adithaklm <94104632+Adithaklm@users.noreply.github.com>
2022-02-06 19:53:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -439,7 +439,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons = [[
             InlineKeyboardButton('🤖 Updates', url='https://t.me/fh_bot_updates'),
-            InlineKeyboardButton('👨‍💻Ｄｅｖ👨‍💻', url='https://t.me/fh_bot_updates')
+            InlineKeyboardButton('👨‍💻Ｄｅｖ👨‍💻', url='https://t.me/my_devs')
         ], [
             InlineKeyboardButton('🏠 Home', callback_data='start'),
             InlineKeyboardButton('🔐 Close', callback_data='close_data')
