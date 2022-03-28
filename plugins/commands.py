09b861f10baec6215ccbdcc17c73d112b9b87e16
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2022-03-28 15:47:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('💠 SΞARCH HΞRΞ 💠', switch_inline_query_current_chat='')
+            InlineKeyboardButton('💠 SΞARCH HΞRΞ 💠', callback_data='help')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_text(
