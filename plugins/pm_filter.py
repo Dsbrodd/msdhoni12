87cb24d4f66c335b11794bb52452f0990b9194ed
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2022-03-31 00:06:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -436,9 +436,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('🔐 𝗖ʟᴏ𝘀ᴇ', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.reply_sticker(
-            sticker="CAACAgUAAxkBAAEBxBhiRKABr6uP_mqlQJ2dmlnfRThqSAACBAADwSQxMYnlHW4Ls8gQHgQ",
+        await query.message.edit_text(
+            text=script.HELP_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
+            parse_mode='html'
         )
     elif query.data == "about":
         buttons = [[
