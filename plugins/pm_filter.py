9d1c8c8b16c49ceb8ffab4c6ad963450f2471aae
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2022-04-01 21:35:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -438,7 +438,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action("typing")
-      m=await query.message.reply_sticker("CAACAgUAAxkBAAPLYhmLeQjtiqPZJEHOFh4KFOjbWzcAAvgDAAKfRIlXnd2oxpsLJeYeBA") 
+        m=await query.message.reply_sticker("CAACAgUAAxkBAAPLYhmLeQjtiqPZJEHOFh4KFOjbWzcAAvgDAAKfRIlXnd2oxpsLJeYeBA") 
         await asyncio.sleep(1) await m.delete()
         await query.message.edit_text(
             text=script.HELP_TXT.format(query.from_user.mention),
